ad6de08caa7a9cb60003ab39af42f93b5cd54045
easyhz <jiton3@naver.com>
2024-06-11 15:21:44 +0900

    Chore: :core:common 모듈 추가

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.daypet.android.library)
+    alias(libs.plugins.daypet.android.library.compose)
+    alias(libs.plugins.daypet.android.application.test)
+}
+
+android {
+    namespace = "com.easyhz.daypet.common"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+
+}
