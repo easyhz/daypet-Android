5add0ff14b6625f2f488893c48ca8d74d86b925c
easyhz <jiton3@naver.com>
2024-06-12 20:49:51 +0900

    chore:  glide, icons 라이브러리 추가

--- build-logic/convention/src/main/java/com/easyhz/daypet/buildlogic/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/java/com/easyhz/daypet/buildlogic/convention/AndroidCompose.kt
@@ -26,6 +26,7 @@ internal fun Project.configureAndroidCompose(
             add("implementation", libs.findLibrary("androidx.ui.tooling.preview").get())
             add("implementation", libs.findLibrary("androidx.material").get())
             add("implementation", libs.findLibrary("androidx.material3").get())
+            add("implementation", libs.findLibrary("androidx.material.icons").get())
             add("implementation", libs.findLibrary("androidx.navigation.compose").get())
         }
     }
