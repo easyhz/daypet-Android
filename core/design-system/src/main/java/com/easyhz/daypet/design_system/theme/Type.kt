44d4f174a034b8ab0b88951242a5461a6330852c
easyhz <jiton3@naver.com>
2024-06-12 20:50:47 +0900

    design: Type 수정

--- core/design-system/src/main/java/com/easyhz/daypet/design_system/theme/Type.kt
+++ core/design-system/src/main/java/com/easyhz/daypet/design_system/theme/Type.kt
@@ -1,9 +1,11 @@
 package com.easyhz.daypet.design_system.theme
 
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.easyhz.daypet.design_system.R
 
@@ -23,15 +25,85 @@ private val LetterSpacing = 0.sp
 val Heading1 = TextStyle(
     fontFamily = AppleSDGothicNeo,
     fontWeight = FontWeight.Bold,
-    color = Primary2,
+    color = TextColor,
     fontSize = 22.sp,
     lineHeight = 22.sp,
     letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    )
+)
+
+val Heading2 = TextStyle(
+    fontFamily = AppleSDGothicNeo,
+    fontWeight = FontWeight.Bold,
+    color = TextColor,
+    fontSize = 18.sp,
+    lineHeight = 18.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    )
 )
 
 val Body1 = TextStyle(
     fontFamily = AppleSDGothicNeo,
     fontWeight = FontWeight.Medium,
     fontSize = 16.sp,
+    color = TextColor,
     letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+)
+
+val Body2 = TextStyle(
+    fontFamily = AppleSDGothicNeo,
+    fontWeight = FontWeight.SemiBold,
+    color = TextColor,
+    fontSize = 14.sp,
+    letterSpacing = LetterSpacing
+)
+
+
+val SubHeading1 = TextStyle(
+    fontFamily = AppleSDGothicNeo,
+    fontWeight = FontWeight.Normal,
+    color = TextColor,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    )
+)
+
+val SubBody1 = TextStyle(
+    fontFamily = AppleSDGothicNeo,
+    fontWeight = FontWeight.Medium,
+    color = SubTextColor,
+    fontSize = 14.sp,
+    lineHeight = 14.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    )
+)
+
+val SubBody2 = TextStyle(
+    fontFamily = AppleSDGothicNeo,
+    fontWeight = FontWeight.Normal,
+    color = SubTextColor,
+    fontSize = 12.sp,
+    lineHeight = 12.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    )
 )
