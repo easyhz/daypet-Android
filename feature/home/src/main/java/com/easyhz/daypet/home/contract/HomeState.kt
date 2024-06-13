2400667af82a0d4f8c99efa0d9f94ec00a701a09
easyhz <jiton3@naver.com>
2024-06-13 13:26:12 +0900

    refactor: HomeState 변수명 및 타입 수정

--- feature/home/src/main/java/com/easyhz/daypet/home/contract/HomeState.kt
+++ feature/home/src/main/java/com/easyhz/daypet/home/contract/HomeState.kt
@@ -1,19 +1,20 @@
 package com.easyhz.daypet.home.contract
 
 import com.easyhz.daypet.common.base.UiState
+import com.easyhz.daypet.domain.model.event.Archive
 import com.easyhz.daypet.domain.model.event.Task
 import com.easyhz.daypet.home.view.event.Event
 import java.time.LocalDate
 
 data class HomeState(
-    val eventList: List<Event>,
+    val archiveList: List<Archive>,
     val taskList: List<Task>,
     val isLoading: Boolean,
     val selection: LocalDate
 ) : UiState() {
     companion object {
         fun init() = HomeState(
-            eventList = emptyList(),
+            archiveList = emptyList(),
             taskList = emptyList(),
             isLoading = false,
             selection = LocalDate.now()
