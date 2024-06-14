cd4a94460d7249df1c3de7e8a196da27ed0fb9e2
easyhz <jiton3@naver.com>
2024-06-14 18:37:43 +0900

    refactor: HomeIntent 이름 변경

--- feature/home/src/main/java/com/easyhz/daypet/home/contract/HomeIntent.kt
+++ feature/home/src/main/java/com/easyhz/daypet/home/contract/HomeIntent.kt
@@ -3,12 +3,11 @@ package com.easyhz.daypet.home.contract
 import com.easyhz.daypet.common.base.UiIntent
 import com.easyhz.daypet.domain.model.event.Archive
 import com.easyhz.daypet.domain.model.event.Task
-import com.easyhz.daypet.home.view.event.Event
 import java.time.LocalDate
 
 sealed class HomeIntent: UiIntent() {
     data class ChangeDate(val clickedDay: LocalDate): HomeIntent()
-    data class ArchiveClick(val archive: Archive): HomeIntent()
-    data class TaskClick(val task: Task): HomeIntent()
+    data class ClickArchive(val archive: Archive): HomeIntent()
+    data class ClickTask(val task: Task): HomeIntent()
 
 }
