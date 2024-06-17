1e8036cb8a47aa0f95182bff40bebce16e469957
easyhz <jiton3@naver.com>
2024-06-17 22:08:06 +0900

    refactor: archive -> memory , task -> todo 변경

--- domain/src/main/java/com/easyhz/daypet/domain/model/memory/Memory.kt
+++ domain/src/main/java/com/easyhz/daypet/domain/model/memory/Memory.kt
@@ -1,6 +1,6 @@
-package com.easyhz.daypet.domain.model.event
+ package com.easyhz.daypet.domain.model.memory
 
-data class Archive(
+data class Memory(
     val title: String,
     val imageUrl: String,
     val time: String
