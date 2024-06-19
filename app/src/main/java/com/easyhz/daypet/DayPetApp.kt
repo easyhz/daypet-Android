f064930e7f754a99648749c6325a58dcf435a651
easyhz <jiton3@naver.com>
2024-06-19 15:29:08 +0900

    design: transition 애니메이션 추가

--- app/src/main/java/com/easyhz/daypet/DayPetApp.kt
+++ app/src/main/java/com/easyhz/daypet/DayPetApp.kt
@@ -1,5 +1,7 @@
 package com.easyhz.daypet
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -10,12 +12,18 @@ import com.easyhz.daypet.navigation.home.navigateToUploadMemory
 import com.easyhz.daypet.navigation.memory_detail.memoryDetailScreen
 import com.easyhz.daypet.navigation.upload_memory.uploadMemoryScreen
 
+const val DURATION = 500
+
 @Composable
 fun DayPetApp() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = Home
+        startDestination = Home,
+        enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start, tween(DURATION)) },
+        exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Start, tween(DURATION)) },
+        popEnterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.End, tween(DURATION)) },
+        popExitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End, tween(DURATION)) }
     ) {
         homeScreen(
             navigateToMemoryDetail = navController::navigateToMemoryDetail,
