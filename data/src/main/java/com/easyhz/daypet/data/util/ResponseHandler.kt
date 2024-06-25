646349fa5521da667c9209ecd4a8b99501526ded
easyhz <jiton3@naver.com>
2024-06-25 18:39:39 +0900

    refactor: 디스패처 파라미터로 전달

--- data/src/main/java/com/easyhz/daypet/data/util/ResponseHandler.kt
+++ data/src/main/java/com/easyhz/daypet/data/util/ResponseHandler.kt
@@ -7,15 +7,17 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 const val TAG = "ResponseHandler"
 
-suspend inline fun < reified T: Any> collectionHandler(
+internal suspend inline fun < reified T: Any> collectionHandler(
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<QuerySnapshot>
-): Result<List<T>> = withContext(Dispatchers.IO) {
+): Result<List<T>> = withContext(dispatcher) {
     try {
         val result = execute().await().toObjects(T::class.java)
         Result.success(result)
@@ -28,9 +30,10 @@ suspend inline fun < reified T: Any> collectionHandler(
     }
 }
 
-suspend inline fun <reified T> documentHandler(
+internal suspend inline fun <reified T> documentHandler(
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<DocumentSnapshot>
-): Result<T> = withContext(Dispatchers.IO) {
+): Result<T> = withContext(dispatcher) {
     try {
         val result = execute().await().toObject(T::class.java)
         result?.let {
