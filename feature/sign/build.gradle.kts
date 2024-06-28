307168cb38cabf448f57c6fbb99ab48b8ce5f8af
easyhz <jiton3@naver.com>
2024-06-28 16:06:00 +0900

    chore: sign 모듈 추가

---
+++ feature/sign/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.daypet.android.library.compose)
+    alias(libs.plugins.daypet.android.feature)
+    alias(libs.plugins.daypet.android.hilt)
+}
+
+android {
+    namespace = "com.easyhz.daypet.sign"
+}
+
+dependencies {
+    implementation(projects.domain)
+    implementation(projects.core.common)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
