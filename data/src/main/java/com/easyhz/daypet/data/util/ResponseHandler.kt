3e985fa3cdca0a69c544540b933300d6d7cbf978
easyhz <jiton3@naver.com>
2024-07-04 16:43:26 +0900

    refactor: handler refactor

--- data/src/main/java/com/easyhz/daypet/data/util/ResponseHandler.kt
+++ data/src/main/java/com/easyhz/daypet/data/util/ResponseHandler.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.easyhz.daypet.common.error.DayPetError
 import com.easyhz.daypet.common.error.getErrorByCode
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
@@ -14,6 +15,11 @@ import kotlinx.coroutines.withContext
 
 const val TAG = "ResponseHandler"
 
+/**
+ * collection 에서 원하는 document 리스트를 받는 함수
+ *
+ * @param execute [QuerySnapshot] 파이어스토어 쿼리문이 들어옴.
+ */
 internal suspend inline fun < reified T: Any> collectionHandler(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<QuerySnapshot>
@@ -26,6 +32,11 @@ internal suspend inline fun < reified T: Any> collectionHandler(
     )
 }
 
+/**
+ * collection 에서 원하는 document 를 받는 함수
+ *
+ * @param execute [DocumentSnapshot] 파이어스토어 쿼리문이 들어옴.
+ */
 internal suspend inline fun <reified T> documentHandler(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<DocumentSnapshot>
@@ -39,7 +50,12 @@ internal suspend inline fun <reified T> documentHandler(
     )
 }
 
-internal suspend inline fun writeHandler(
+/**
+ * collection 에 document 이름을 지정하고 저장하는 함수
+ *
+ * @param execute document id 를 지정한 set 쿼리문이 들어옴.
+ */
+internal suspend inline fun setHandler(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<Void>
 ): Result<Unit> = withContext(dispatcher) {
@@ -51,6 +67,29 @@ internal suspend inline fun writeHandler(
     )
 }
 
+/**
+ * collection 에 저장하고 document id 를 받는 함수
+ *
+ * @param execute [DocumentReference] add 쿼리문이 들어옴.
+ */
+internal suspend inline fun addAndGetIdHandler(
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    crossinline execute: () -> Task<DocumentReference>
+): Result<String> = withContext(dispatcher) {
+    runCatching {
+        val result = execute().await()
+        result.id
+    }.fold(
+        onSuccess = { Result.success(it) },
+        onFailure = { e -> handleException(e, "addAndGetId") }
+    )
+}
+
+/**
+ * collection 에 원하는 document 존재 여부를 리턴하는 함수
+ *
+ * @param execute [DocumentSnapshot] get 쿼리문이 들어옴.
+ */
 internal suspend inline fun existHandler(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline execute: () -> Task<DocumentSnapshot>
