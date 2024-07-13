fea4b14250c9ef6384905077abdbc77c3a5cbf51
easyhz <jiton3@naver.com>
2024-07-13 16:44:00 +0900

    fix: theme 수정

--- core/design-system/src/main/java/com/easyhz/daypet/design_system/theme/Theme.kt
+++ core/design-system/src/main/java/com/easyhz/daypet/design_system/theme/Theme.kt
@@ -25,7 +25,6 @@ fun DayPetTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
