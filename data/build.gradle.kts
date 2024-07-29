2f57ade336cbc76171d3d42ab10e46cc65ae6a44
easyhz <jiton3@naver.com>
2024-07-29 23:37:00 +0900

    chore: buildconfig 설정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.daypet.android.library)
     alias(libs.plugins.daypet.android.hilt)
@@ -5,8 +7,19 @@ plugins {
     alias(libs.plugins.daypet.android.application.firebase)
 }
 
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
 android {
     namespace = "com.easyhz.daypet.data"
+    buildTypes {
+        debug {
+            buildConfigField("String", "EMPTY_URL", localProperties["empty.url"].toString())
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
