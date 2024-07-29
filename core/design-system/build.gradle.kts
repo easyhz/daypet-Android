099e1d208d4aa7bb29aae8295cb18b3c80cae6f8
easyhz <jiton3@naver.com>
2024-07-29 22:08:39 +0900

    chore: empty url 추가

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -1,11 +1,24 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.daypet.android.library)
     alias(libs.plugins.daypet.android.library.compose)
     alias(libs.plugins.daypet.android.application.test)
 }
 
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
 android {
     namespace = "com.easyhz.daypet.design_system"
+    buildTypes {
+        debug {
+            buildConfigField("String", "EMPTY_URL", localProperties["empty.url"].toString())
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
