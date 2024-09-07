af5e361fb6fe3ee9fb3c182b5e1f0a223f8018b1
easyhz <jiton3@naver.com>
2024-09-07 19:16:15 +0900

    feat: daypet fcm 추가

--- app/src/main/java/com/easyhz/daypet/DayPetApplication.kt
+++ app/src/main/java/com/easyhz/daypet/DayPetApplication.kt
@@ -1,6 +1,11 @@
 package com.easyhz.daypet
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.easyhz.daypet.data.service.NotificationService
 import com.easyhz.daypet.work.DayPetWorkManger
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,5 +14,19 @@ class DayPetApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         DayPetWorkManger.scheduleCameraCacheCleanWork(this)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NotificationService.CHANNEL_ID,
+                NotificationService.CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH,
+            )
+            channel.description = "DayPet Notification Channel"
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- data/src/main/java/com/easyhz/daypet/data/service/CloudMessagingService.kt
+++ data/src/main/java/com/easyhz/daypet/data/service/CloudMessagingService.kt
@@ -1,6 +1,8 @@
 package com.easyhz.daypet.data.service
 
+import android.content.Intent
 import android.util.Log
+import com.google.firebase.messaging.Constants
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,5 +23,18 @@ class CloudMessagingService: FirebaseMessagingService() {
         super.onMessageReceived(message)
         Log.d("FCM SERVICE ", "message data: ${message.data}")
         notificationService.showNotification(message)
+    }
+
+    override fun handleIntent(intent: Intent?) {
+        val new = intent?.apply {
+            val temp = extras?.apply {
+                remove(Constants.MessageNotificationKeys.ENABLE_NOTIFICATION)
+                remove("gcm.notification.title")
+                remove("gcm.notification.e")
+                remove("gcm.notification.body")
+            }
+            replaceExtras(temp)
+        }
+        super.handleIntent(new)
     }
 }
